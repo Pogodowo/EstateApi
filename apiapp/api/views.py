e28f80633a5d54a904a2d1d5a3deb2289010453a
Lukasz <lukabr@op.pl>
2023-03-09 10:29:56 +0100

    dodałem null

--- apiapp/api/views.py
+++ apiapp/api/views.py
@@ -7,6 +7,7 @@ from .serializers import  PostModelSerializer
 from ..models import baza_ogloszen
 from django.utils import timezone
 import datetime
+import sys
 
 @api_view(['GET','POST'])
 def ogl_list(request):
@@ -17,6 +18,7 @@ def ogl_list(request):
     elif request.method == 'POST':
         serializer = PostModelSerializer(data = request.data)
         print('coś działa')
+        sys.stdout.flush()
         baza_ogloszen.objects.create(tytul=request.data['tytul'], url_link=request.data['url_link'],
                                      lokalizacja=request.data['lokalizacja'], powierzchnia=request.data['powierzchnia'],
                                      cena=request.data['cena'], cena_za_metr=request.data['cena_za_metr'],
--- apiapp/models.py
+++ apiapp/models.py
@@ -1,14 +1,14 @@
 from django.db import models
 
 class baza_ogloszen(models.Model):
-    tytul = models.CharField(max_length=80)
-    lokalizacja = models.CharField(max_length=80)
-    cena = models.CharField(max_length=30)
-    cena_za_metr = models.CharField(max_length=30)
-    liczba_pokoi = models.CharField(max_length=30)
-    powierzchnia = models.CharField(max_length=20)
-    url_link = models.TextField(max_length=200)
-    data_wystawienia = models.DateTimeField()
+    tytul = models.CharField(max_length=80,null=True,blank=True)
+    lokalizacja = models.CharField(max_length=80,null=True,blank=True)
+    cena = models.CharField(max_length=30,null=True,blank=True)
+    cena_za_metr = models.CharField(max_length=30,null=True,blank=True)
+    liczba_pokoi = models.CharField(max_length=30,null=True,blank=True)
+    powierzchnia = models.CharField(max_length=20,null=True,blank=True)
+    url_link = models.TextField(max_length=200,null=True,blank=True)
+    data_wystawienia = models.DateTimeField(null=True,blank=True)
     data_zakonczenia = models.DateTimeField(null=True,blank=True)
     def __str__(self):
         return self.tytul
