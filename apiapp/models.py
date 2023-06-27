ede6251380cf0b4663271daf289f57802179e17a
Lukasz <lukabr@op.pl>
2023-06-27 21:15:02 +0200

    dodałem null do ulubione

--- apiapp/models.py
+++ apiapp/models.py
@@ -12,7 +12,7 @@ class baza_ogloszen(models.Model):
     data_wystawienia = models.DateTimeField(null=True,blank=True)
     data_zakonczenia = models.DateTimeField(null=True,blank=True)
     id = models.AutoField(primary_key=True)
-    ulubione=models.CharField(max_length=3,default='off')
+    ulubione=models.CharField(max_length=3,default='off',null=True,blank=True)
     def __str__(self):
         return self.tytul
 
