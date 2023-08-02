ee79f19b6c3a2dd4c7dc91db5cde296d1c835bed
Lukasz <lukabr@op.pl>
2023-08-02 19:18:48 +0200

    wydłużyłem url

--- apiapp/models.py
+++ apiapp/models.py
@@ -7,7 +7,7 @@ class baza_ogloszen(models.Model):
     cena_za_metr = models.CharField(max_length=30,null=True,blank=True)
     liczba_pokoi = models.CharField(max_length=30,null=True,blank=True)
     powierzchnia = models.CharField(max_length=20,null=True,blank=True)
-    url_link = models.TextField(max_length=200,null=True,blank=True)
+    url_link = models.TextField(max_length=300,null=True,blank=True)
     foto = models.TextField(max_length=300, null=True, blank=True)
     data_wystawienia = models.DateTimeField(null=True,blank=True)
     data_zakonczenia = models.DateTimeField(null=True,blank=True)
